fc672f59cbf4f018fa5de896a3a5acf8bec19548
Sumangala Patki <40850265+sumangala17@users.noreply.github.com>
2024-06-23 02:23:39 -0700

    Update ProfileCard.tsx

--- src/routes/Feed/ProfileCard.tsx
+++ src/routes/Feed/ProfileCard.tsx
@@ -38,7 +38,7 @@ const StyledWrapper = styled.div`
     border-radius: 1rem;
     width: 100%;
     background-color: ${({ theme }) =>
-      theme.scheme === "light" ? theme.colors.purple6 : theme.colors.purple6};
+      theme.scheme === "light" ? theme.colors.gray6 : theme.colors.gray6};
     @media (min-width: 768px) {
       padding: 1rem;
     }
